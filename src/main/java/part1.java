ab1c9340cd5be4cc98e4e29b676cf9ad5dbc9280
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-01-18 06:45:02 +0900

    TIL test

---
+++ src/main/java/part1.java
@@ -0,0 +1,14 @@
+public class part1 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    String aa;
+
+    @Override
+    public String toString() {
+        return "part1{" +
+                "aa='" + aa + '\'' +
+                '}';
+    }
+}
